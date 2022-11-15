6800f5799ce2781cb53e8fbde3a5ec6f12de95e3
Yurii <paraska108@gmail.com>
2022-11-15 19:48:36 +0200

    clean and improve, finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import NaviBar from './Components/NaviBar';
 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from './Home';
 import About from './About';
 
-// import { Button } from 'react-bootstrap';
 function App() {
   return (
     <>
       <BrowserRouter>
       <NaviBar />
         <Routes>
-          <Route key={1} path="/" element={<Home />} />
-          <Route key={2} path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </>
